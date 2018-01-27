4b6e406b45875ce5d7ded38f4ad6a46f096b8426
David Clark <dlc.clark@gmail.com>
2018-01-27 02:21:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,15 @@ function init() {
   document.body.addEventListener('keydown', function(e){
     const key = parseInt(e.detail || e.which);
     if(key === code[index]){
-      index++
+      index++;
+      
+      if(index === code.length){
+        alert("Hurray!");
+        
+        index = 0;
+      }
+    } else {
+      index = 0;
     }
   })
 }
